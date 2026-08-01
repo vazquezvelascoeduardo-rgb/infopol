6fcad39def2627e67ecaffde1f35184b220e6e07
vazquezvelascoeduardo-rgb <vazquezvelascoeduardo@gmail.com>
2026-08-01 16:33:13 +0200

    Fora els actes de la PL de Viladecans
    Eren impresos d'un sol ajuntament dins d'una app per a tot Catalunya:
    320 blocs escampats per 36 checklists de penal i trànsit, més una pàgina
    pròpia a Operativa. Per a qui no és de Viladecans no volien dir res.
    
    Marxen els blocs, l'entrada del renderitzador, la pàgina, la ruta i el
    fitxer de dades. Queden les notes de model de custòdia i d'instrucció
    d'atestats, que no són actes i que ara mateix no es pinten enlloc.

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,6 @@ const PenalTaulaActes = lazy(() => import('./pages/operativa/PenalTaulaActes'));
 const PenalTaulaDrogues = lazy(() => import('./pages/operativa/PenalTaulaDrogues'));
 const PenalRecursos = lazy(() => import('./pages/operativa/PenalRecursos'));
 const PenalDretsDetingut = lazy(() => import('./pages/operativa/PenalDretsDetingut'));
-const ActesViladecans = lazy(() => import('./pages/operativa/ActesViladecans'));
 const Superbuscador = lazy(() => import('./pages/Superbuscador'));
 const Recursos = lazy(() => import('./pages/Recursos'));
 const CalculadoraAlcohol = lazy(() => import('./pages/CalculadoraAlcohol'));
@@ -128,9 +127,7 @@ export default function App() {
               <Route path="/operativa/penal/taula-actes" element={<PenalTaulaActes />} />
               <Route path="/operativa/penal/taula-drogues" element={<PenalTaulaDrogues />} />
               <Route path="/operativa/penal/recursos" element={<PenalRecursos />} />
-              <Route path="/operativa/penal/drets-detingut" element={<PenalDretsDetingut />} />
-              <Route path="/operativa/actes-viladecans" element={<ActesViladecans />} />
-              <Route path="/operativa/penal/*" element={<Penal />} />
+              <Route path="/operativa/penal/drets-detingut" element={<PenalDretsDetingut />} />              <Route path="/operativa/penal/*" element={<Penal />} />
               <Route path="/superbuscador" element={<Superbuscador />} />
               <Route path="/calculadora-alcohol" element={<CalculadoraAlcohol />} />
               <Route path="/leyes" element={<Leyes />} />
